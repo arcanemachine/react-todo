d95976493d839474d2e65e38df5f79ea0f831975
Nicholas Moen <arcanemachine@tutanota.com>
2021-06-09 21:39:47 -0600

    Convert TodoItems to functional component

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import TodoItem from './TodoItem.js';
 
-export default class TodoItems extends React.Component {
-  render() {
-    const todos = this.props.todos.length ? (
-      this.props.todos.map((todo) => {
-        return (
-          <TodoItem key={todo.id}
-                    todo={todo}
-                    className="mt-4"
-                    currentUpdatePanel={this.props.currentUpdatePanel}
-                    emitSetCurrentUpdatePanel=
-                      {this.props.emitSetCurrentUpdatePanel}
-                    emitTodoUpdate={this.props.emitTodoUpdate}
-                    emitTodoDelete={this.props.emitTodoDelete} />
-        );
-      })
-    ) : (
-      <li className="my-4">
-        Your todo list is empty.
-      </li>
-    );
-    return (
-      <div className="has-text-centered">
-        <ul className="my-4 py-2 box">
-          {todos}
-        </ul>
-        <div className="my-4">
-          You can modify or remove an item by clicking on it.
-        </div>
+export default function TodoItems(props) {
+  const todos = props.todos.length ? (
+    props.todos.map((todo) => {
+      return (
+        <TodoItem key={todo.id}
+                  todo={todo}
+                  className="mt-4"
+                  currentUpdatePanel={props.currentUpdatePanel}
+                  emitSetCurrentUpdatePanel=
+                    {props.emitSetCurrentUpdatePanel}
+                  emitTodoUpdate={props.emitTodoUpdate}
+                  emitTodoDelete={props.emitTodoDelete} />
+      );
+    })
+  ) : (
+    <li className="my-4">
+      Your todo list is empty.
+    </li>
+  );
+
+  return (
+    <div className="has-text-centered">
+      <ul className="my-4 py-2 box">
+        {todos}
+      </ul>
+      <div className="my-4">
+        You can modify or remove an item by clicking on it.
       </div>
-    );
-  }
+    </div>
+  );
 }
