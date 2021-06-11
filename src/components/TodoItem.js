338acbb5d1474fca7e82ff29e0a9694ddc5bea6f
Nicholas Moen <arcanemachine@tutanota.com>
2021-06-10 19:27:22 -0600

    All components converted to stateful function-based components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,81 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class TodoItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      updatedDescription: '',
-    }
+export default function NewTodoItem(props) {
+  const [updatedDescription, setUpdatedDescription] = useState('');
 
-    // events
-    this.emitTodoDelete = this.emitTodoDelete.bind(this);
-    this.handleChangeUpdatedDescription = 
-      this.handleChangeUpdatedDescription.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.todoUpdatePanelEnable = this.todoUpdatePanelEnable.bind(this);
-    this.todoUpdatePanelDisable = this.todoUpdatePanelDisable.bind(this);
-
-    // refs
-    this.inputUpdatedDescription = React.createRef();
+  function emitTodoDelete() {
+    todoUpdatePanelDisable();
+    props.emitTodoDelete(props.todo.id);
   }
 
-  emitTodoDelete(todo) {
-    this.todoUpdatePanelDisable();
-    this.props.emitTodoDelete(todo);
-  }
-
-  handleChangeUpdatedDescription(e) {
-    this.setState({ updatedDescription: e.target.value })
-  }
-
-  handleSubmit(e) {
+  function handleUpdate(e) {
     e.preventDefault();
-    this.todoUpdatePanelDisable();
-    this.props.emitTodoUpdate(
-      this.props.todo,
-      this.state.updatedDescription
-    );
+    todoUpdatePanelDisable();
+    props.emitTodoUpdate(props.todo.id, updatedDescription);
   }
 
-  todoUpdatePanelEnable() {
-    console.log('TodoItem: emitted emitSetCurrentUpdatePanel()');
-    this.props.emitSetCurrentUpdatePanel(this.props.todo.id);
-    this.setState({ updatedDescription: this.props.todo.description });
+  function todoUpdatePanelEnable() {
+    props.emitSetCurrentUpdatePanel(props.todo.id);
+    setUpdatedDescription(props.todo.description);
   }
 
-  todoUpdatePanelDisable() {
-    this.props.emitSetCurrentUpdatePanel(undefined);
+  function todoUpdatePanelDisable() {
+    props.emitSetCurrentUpdatePanel(undefined);
   }
 
-  render() {
-    return (
-      this.props.currentUpdatePanel === this.props.todo.id ? (
-        <form className="my-4"
-              onSubmit={this.handleSubmit}>
-          <input type="button"
-                 onClick={this.emitTodoDelete}
-                 value="Delete" />
-          <input type="text"
-                 className="ml-2"
-                 ref={this.inputUpdatedDescription}
-                 value={this.state.updatedDescription}
-                 onChange={this.handleChangeUpdatedDescription}
-                 autoFocus />
-          <input type="submit"
-                 className="ml-2"
-                 value="Confirm" />
-          <input type="button"
-                 className="ml-2"
-                 value="Cancel"
-                 onClick={this.todoUpdatePanelDisable} />
-        </form>
-      ) : (
-        <li className="my-4 cursor-pointer"
-            onClick={this.todoUpdatePanelEnable}>
-          {this.props.todo.description}
-        </li>
-      )
-    );
-  }
+  return (
+    props.currentUpdatePanel === props.todo.id ? (
+      <form className="my-4"
+            onSubmit={handleUpdate}>
+        <input type="button"
+               onClick={emitTodoDelete}
+               value="Delete" />
+        <input type="text"
+               className="ml-2"
+               value={updatedDescription}
+               onChange={(e) => setUpdatedDescription(e.target.value)}
+               autoFocus />
+        <input type="submit"
+               className="ml-2"
+               value="Confirm" />
+        <input type="button"
+               className="ml-2"
+               value="Cancel"
+               onClick={todoUpdatePanelDisable} />
+      </form>
+    ) : (
+      <li className="my-4 cursor-pointer"
+          onClick={todoUpdatePanelEnable}>
+        {props.todo.id + 1}. {props.todo.description}
+      </li>
+    )
+  );
 }
-
--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -2,29 +2,36 @@ import React from 'react';
 import TodoItem from './TodoItem.js';
 
 export default function TodoItems(props) {
-  const todos = props.todos.length ? (
-    props.todos.map((todo) => {
+  function getTodos() {
+    if (props.todos.length) {
       return (
-        <TodoItem key={todo.id}
-                  todo={todo}
-                  className="mt-4"
-                  currentUpdatePanel={props.currentUpdatePanel}
-                  emitSetCurrentUpdatePanel=
-                    {props.emitSetCurrentUpdatePanel}
-                  emitTodoUpdate={props.emitTodoUpdate}
-                  emitTodoDelete={props.emitTodoDelete} />
+        props.todos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              className="mt-4"
+              currentUpdatePanel={props.currentUpdatePanel}
+              emitSetCurrentUpdatePanel=
+                {props.emitSetCurrentUpdatePanel}
+              emitTodoUpdate={props.emitTodoUpdate}
+              emitTodoDelete={props.emitTodoDelete} />
+          );
+        })
+      )
+    } else {
+      return (
+        <li className="my-4">
+          Your todo list is empty.
+        </li>
       );
-    })
-  ) : (
-    <li className="my-4">
-      Your todo list is empty.
-    </li>
-  );
+    }
+  }
 
   return (
     <div className="has-text-centered">
       <ul className="my-4 py-2 box">
-        {todos}
+        {getTodos()}
       </ul>
       <div className="my-4">
         You can modify or remove an item by clicking on it.
